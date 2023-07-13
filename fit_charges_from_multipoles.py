9dcd817a373a2eaca5193e6aa6bf54e6019e54fb
Ethan Bull-Vulpe <ebullvul@tscc-login2.sdsc.edu>
2023-07-12 22:16:37 -0700

    Cleaned up code with argparse and better error/warning messages

--- fit_charges_from_multipoles.py
+++ fit_charges_from_multipoles.py
@@ -9,6 +9,7 @@ import scipy.optimize as optimize
 from scipy.optimize import LinearConstraint
 import random
 import sympy
+import argparse
 
 global XYZ    # List of lists with coordinates of the training set elements
 global N      # Max multipole to fit
@@ -144,10 +145,6 @@ def get_spherical_harmonics_multipoles(charges: Sequence[float], configuration: 
                 # print(f"coeff: {coeff}")
                 for j in range(0, len(charges)):
                     multipole_moments[l][multipole_index-offset-1] += coeff*charges[j] * (configuration[j][0]**lx) * (configuration[j][1]**ly) * (configuration[j][2]**lz)
-            
-                # if lz == 2:
-                #     for j in range(0, len(coords)):
-                #         multipole_moments[n-1] -= coeff*(1/3)*charges[j] * (coords[j][0]**2 + coords[j][1]**2 + coords[j][2]**2)
 
 
             # multipole_moments[l][n-offset-1] *= 4.803 # not sure why need 0.5 factor
@@ -298,16 +295,11 @@ def penalty_function(charges: Sequence[float], training_set: Sequence[Sequence[T
     
     for n, (configuration, ref_multipoles) in enumerate(zip(training_set, reference_multipoles)):        
         # Calculates the multipoles for the given charges
-        # mpcalc = get_multipoles(params,XYZ[n],N)
         mpcalc = get_spherical_harmonics_multipoles(charges, configuration, max_multipole_level)
-
-        # print("Current Multipoles:", mpcalc)
-        # print("Reference Multipoles:", ref_multipoles)
 
         # Get the residual as a sum of squares of the differences in each multipole
         for i in range(max_multipole_level + 1):
             # weight = 100/(i+1)**8
-            # weight = 1 if i < 2 else 0
             weight = 1
             for j in range(len(mpcalc[i])):
                 residual += weight*(mpcalc[i][j] - ref_multipoles[i][j])*(mpcalc[i][j] - ref_multipoles[i][j])
@@ -319,24 +311,14 @@ def penalty_function(charges: Sequence[float], training_set: Sequence[Sequence[T
         
 def get_linearly_dependant_constraints(constraint_matrix: Sequence[Sequence[float]], zero_tolerance: float = 1e-3) -> Sequence[int]:
     
-    # Find the indices of all linearly independent rows
-    # _, linearly_independent_row_indices = sympy.Matrix(constraint_matrix).T.rref(iszerofunc = lambda x: x < zero_tolerance)
-    # print("Rank:", numpy.linalg.matrix_rank(constraint_matrix, tol=1e-3))
-    
     linearly_dependent_row_indices: MutableSequence[int] = []
     
     for test_index in range(0, len(constraint_matrix)):
-        # print(linearly_dependent_row_indices)
         test_matrix: Sequence[Sequence[float]] = [row for index, row in enumerate(constraint_matrix) if index <= test_index and index not in linearly_dependent_row_indices]
-        # print(test_matrix)
-        
-        # print(numpy.linalg.matrix_rank(test_matrix, tol=zero_tolerance), len(test_matrix))
         
         if numpy.linalg.matrix_rank(test_matrix, tol=zero_tolerance) < len(test_matrix):
             linearly_dependent_row_indices.append(test_index)
-    
-    # print(linearly_independent_row_indices)
-    
+            
     # The rows that are not linearly independent must be dependent.
     # return [row_index for row_index in range(len(constraint_matrix)) if row_index not in linearly_independent_row_indices]
     return linearly_dependent_row_indices
@@ -362,10 +344,6 @@ def get_stewart_constraints(
     
     while True:
         
-        print(f"{l = } {offset = }")
-        print(f"num constraints: {len(constraint_matrix) + len(stewart_constraint_matrix)}")
-        print(f"remaining degrees of freedom: {remaining_degrees_of_freedom}")
-        
         new_constraint_matrix: MutableSequence[MutableSequence[float]] = [[0.0 for _ in configuration] for m in range(-l, l+1)]
         new_constraint_minimums: MutableSequence[float] = [0.0 for m in range(-l, l+1)]
         new_constraint_maximums: MutableSequence[float] = [0.0 for m in range(-l, l+1)]
@@ -378,14 +356,10 @@ def get_stewart_constraints(
                 multipole_index = i+1-2*m
             else:
                 multipole_index = i+2*m
-                
-            # print(f"TEST: {m = }, {multipole_index = }")
             
             for k in range(lfuncc(0, l-1)+1, lfuncc(0, l) + 1):
                 lx, ly, lz = konk2l(k)
                 coeff = ctopsh(l, m, lx, ly, lz)
-                
-                # print(f"{lx =} {ly = } {lz = } {coeff = }")
                 
                 for j in range(0, len(configuration)):
                     new_constraint_matrix[multipole_index-offset-1][j] += coeff * (configuration[j][0]**lx) * (configuration[j][1]**ly) * (configuration[j][2]**lz)
@@ -397,12 +371,14 @@ def get_stewart_constraints(
         test_constraint_matrix.extend(constraint_matrix)
         test_constraint_matrix.extend(stewart_constraint_matrix)
         test_constraint_matrix.extend(new_constraint_matrix)
+
+        # print(f"current constraints {constraint_matrix} {stewart_constraint_matrix}")
         
-        print(f"new candidate constraints: {new_constraint_matrix}, {new_constraint_minimums} {new_constraint_maximums}")
+        # print(f"new candidate constraints: {new_constraint_matrix}, {new_constraint_minimums} {new_constraint_maximums}")
         
         linearly_dependent_constraint_indices = get_linearly_dependant_constraints(test_constraint_matrix)
         
-        print(f"dependent constraints: {[linearly_dependent_constraint_index - len(constraint_matrix) - len(stewart_constraint_matrix) for linearly_dependent_constraint_index in linearly_dependent_constraint_indices]}")
+        # print(f"dependent constraints: {[linearly_dependent_constraint_index - len(constraint_matrix) - len(stewart_constraint_matrix) for linearly_dependent_constraint_index in linearly_dependent_constraint_indices]}")
         
         # problem: indices change as items are removed.
         for removed_items, linearly_dependent_constraint_index in enumerate(linearly_dependent_constraint_indices):
@@ -460,9 +436,13 @@ def fit_multipoles(
             constraint_max: Sequence[float] = []
         ) -> Sequence[float]:
     
+    iteration_count: int = 0
+    
     def callback(charges: Sequence[float], *args: Any) -> None:
+        nonlocal iteration_count
         current_penalty: float = penalty_function(charges, training_set=training_set_configurations, reference_multipoles=reference_multipoles, max_multipole_level=max_multipole_level)
-        print("Current Penalty:", current_penalty)
+        print(f"Iteration {iteration_count} penalty:", current_penalty)
+        iteration_count += 1
         
     def penalty_function_wrapper(charges, arg_dict):
         return penalty_function(charges, arg_dict["training_set"], arg_dict["reference_multipoles"], arg_dict["max_multipole_level"])
@@ -477,78 +457,129 @@ def fit_multipoles(
     
     return final_charges
 
-# MAIN FUNCTION #
+###
+# Main Script Begins Here
+###
 
-if len(sys.argv) != 2:
-    print(f"Usage: python3 {sys.argv[0]} <input.json>")
-    sys.exit()
+### Parse Command Line Arguments
+parser = argparse.ArgumentParser(
+    prog=f"{sys.argv[0]}",
+    description="Fits geometry-independent atomic charges from arbitrarily high multipole moments.",
+    epilog="Please contact the Paesani Group for more information."
+)
 
-json_path = sys.argv[1]
+parser.add_argument(
+        "json_path",
+        help="Path to the input.json input file."
+)
 
-# Read JSON file
+args = parser.parse_args()
+
+json_path: str = args.json_path
+
+### Read JSON file
 with open(json_path,'r') as json_file:
     json_data = json.load(json_file)
 
-# Set up variables
-# Initial guess
-min_guess = json_data["min_guess"]
-max_guess = json_data["max_guess"]
-
 charges: Sequence[float]
 if "charges" in json_data:
-    charges = json_data["charges"]
-else:
-    charges = [min + random.random()*(max-min) for min, max in zip(min_guess, max_guess)]
-print("Initial Guess:", charges)
 
-# Training set
+    if "min_guess" in json_data:
+        print("\"min_guess\" in json_data, but will be ignored since starting charges are specified.")
+        print("\"max_guess\" in json_data, but will be ignored since starting charges are specified.")
+
+    charges = json_data["charges"]
+    print(f"Loading initial charges from json file.")
+elif "min_guess" and "max_guess" not in json_data:
+    print("Please specify either \"charges\", or \"min_guess\" and \"max_guess\" in json input.")
+    sys.exit()
+else:
+    min_guess = json_data["min_guess"]
+    max_guess = json_data["max_guess"]
+    print(f"Randomly initializing charges in ranges {min_guess}-{max_guess}")
+    charges = [min + random.random()*(max-min) for min, max in zip(min_guess, max_guess)]
+print(f"Initial Charges: {charges}")
+
+if "training_set" not in json_data:
+    print("Please specify \"training_set\" in json file.")
+    sys.exit()
+
+print(f"Parsing training set from {json_data['training_set']}...")
 training_set_path = json_data['training_set']
 training_set_configurations, reference_multipoles = read_training_set(training_set_path)
 
 
 # Check for consistency between charges and coordinates
 if len(charges) != len(training_set_configurations[0]):
-    print(f"ERROR: Charges size ({len(charges)}) is not consistent with coordinate size ({len(training_set_configurations[0])})")
+    print(f"ERROR: Charges size ({len(charges)}) is not consistent with atom count ({len(training_set_configurations[0])})")
     sys.exit()
 
-# Max multipole
-N = json_data["n"] 
+print("Multipoles of initial charges:")
+print([[val for val in list] for list in get_spherical_harmonics_multipoles(charges, training_set_configurations[0], len(reference_multipoles[0]) - 1)])
 
-# Check consistency between reference multipoles and multipoles to calculate
-# Ref multiples must be at least same length as N
-if N > len(reference_multipoles[0]):
-    print(f"ERROR: N ({N}) is larger than the reference number of multipoles provided ({len(reference_multipoles[0])})")
+# Parse Constraints
+constraint_matrix: MutableSequence[Sequence[float]]
+constraint_minimums: MutableSequence[float]
+constraint_maximums: MutableSequence[float]
+if "constraint_matrix" in json_data and "constraint_values":
+    print(f"Parsing constraints from json...")
+
+    constraint_matrix = json_data["constraint_matrix"]
+    constraint_minimums = [value - 1e-6 for value in json_data["constraint_values"]]
+    constraint_maximums = [value + 1e-6 for value in json_data["constraint_values"]]
+elif ("constraint_matrix" in json_data and "constraint_values" not in json_data) or ("constraint_matrix" not in json_data and "constraint_values" in json_data):
+    print(f"It looks like one of \"constraint_matrix\" or \"constraint_values\" was specified but not the other. They must both be specified to apply constraints.")
     sys.exit()
+else:
+    print("Did not parse any constraints from json_data.")
 
-# Add constraints, if any
-constraint_matrix: MutableSequence[Sequence[float]] = json_data["constraint_matrix"]
-constraint_minimums: MutableSequence[float] = json_data["constraint_minimums"]
-constraint_maximums: MutableSequence[float] = json_data["constraint_maximums"]
+    constraint_matrix = []
+    constraint_minimums = []
+    constraint_maximums = []
 
 if len(get_linearly_dependant_constraints(constraint_matrix)) > 0:
     print(f"Initial constraint matrix contains linearly dependant constraints.")
     print(f"Matrix:")
     print(f"{constraint_matrix}")
-    print(f"Linearly dependent constraints: {get_linearly_dependant_constraints(constraint_matrix)}")
+    print(f"Linearly dependent constraint indices: {get_linearly_dependant_constraints(constraint_matrix)}")
+    sys.exit()
+
+maximum_fitting_multipole_order: int
+
+add_multipole_constraints: bool = "add_stewart_like_constraints" in json_data and json_data["add_stewart_like_constraints"]
+
+if add_multipole_constraints:
+
+    if "maximum_multipole_order" in json_data:
+        print("\"maximum_multipole_order\" is specified in json input but will be ignored in favor of Stewart-like maximum multipole order.")
     
-stewart_constraint_matrix, stewart_constraint_minimums, stewart_constraint_maximums, stewart_constraint_level, stewart_fitting_level = get_stewart_constraints(
-        training_set_configurations[0],
-        reference_multipoles[0],
-        constraint_matrix,
-        constraint_minimums,
-        constraint_maximums
-)
+    stewart_constraint_matrix, stewart_constraint_minimums, stewart_constraint_maximums, stewart_constraint_level, maximum_fitting_multipole_order = get_stewart_constraints(
+            training_set_configurations[0],
+            reference_multipoles[0],
+            constraint_matrix,
+            constraint_minimums,
+            constraint_maximums
+    )
 
-print(f"Adding {len(stewart_constraint_matrix)} additional Stewart constraints using multipoles up to L={stewart_constraint_level}")
+    print(f"Adding {len(stewart_constraint_matrix)} additional Stewart constraints using multipoles up to L={stewart_constraint_level}")
 
-constraint_matrix.extend(stewart_constraint_matrix)
-constraint_minimums.extend(stewart_constraint_minimums)
-constraint_maximums.extend(stewart_constraint_maximums)
+    constraint_matrix.extend(stewart_constraint_matrix)
+    constraint_minimums.extend(stewart_constraint_minimums)
+    constraint_maximums.extend(stewart_constraint_maximums)
 
-print(f"Will now fit multipoles up to L={stewart_fitting_level}")
+elif "maximum_multipole_order" not in json_data:
+    print(f"Please specify either \"maximum_multipole_order\" or \"add_multipole_constraints\" = True in json input.")
+    sys.exit()
+else:
+    maximum_fitting_multipole_order = json_data["maximum_multipole_order"] 
 
-print("Multipoles of initial charges")
-print([[val for val in list] for list in get_spherical_harmonics_multipoles(charges, training_set_configurations[0], len(reference_multipoles[0]) - 1)])
+# Check consistency between reference multipoles and multipoles to calculate
+# Ref multiples must be at least same length as N
+if maximum_fitting_multipole_order > len(reference_multipoles[0]):
+    print(f"ERROR: N ({maximum_fitting_multipole_order}) is larger than the reference number of multipoles provided ({len(reference_multipoles[0])})")
+    sys.exit()
+
+print(f"Will now fit multipoles up to L={maximum_fitting_multipole_order}")
 
 final_charges: Sequence[float] = fit_multipoles(
         training_set_configurations,
@@ -557,8 +588,22 @@ final_charges: Sequence[float] = fit_multipoles(
         constraint_matrix=constraint_matrix,
         constraint_min=constraint_minimums,
         constraint_max=constraint_maximums,
-        max_multipole_level=stewart_fitting_level
+        max_multipole_level=maximum_fitting_multipole_order
 )
+
+print(f"Fit Complete!")
+
+print("\n\nFitted charges:")
+print([round(float(q),4) for q in final_charges])
+
+print("")
+
+mp = get_spherical_harmonics_multipoles(final_charges, training_set_configurations[0], len(reference_multipoles[0]) - 1)
+
+print("Multipoles of final charges.")
+print([[val for val in list] for list in mp])
+    
+print(f"Total penalty: {penalty_function(final_charges, training_set_configurations, reference_multipoles, max_multipole_level=maximum_fitting_multipole_order)}")
 
 # if len(constraints) != 3:
 #     print("No valid constraints have been found. Running optimization without constraints")
@@ -569,16 +614,10 @@ final_charges: Sequence[float] = fit_multipoles(
 
 # Report
 
-mp = get_spherical_harmonics_multipoles(final_charges, training_set_configurations[0],N)
-print("\n\nResiduals:\n")
+print("\n\nResiduals by moment:")
 
 for i in range(len(mp)):
-    print("i = {}     ".format(i), [mp[i][j] - reference_multipoles[0][i][j] for j in range(len(mp[i]))])
-    
-print(f"Total penalty: {penalty_function(final_charges, training_set_configurations, reference_multipoles, max_multipole_level=stewart_fitting_level)}")
-
-print("\n\nFitted charges:\n")
-print([round(float(q),4) for q in final_charges])
+    print("l = {}     ".format(i), [mp[i][j] - reference_multipoles[0][i][j] for j in range(len(mp[i]))])
 
 # print("\n\nMultipoles:\n")
 # for n in range(len(XYZ)):        
